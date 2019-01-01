90d6510de89063bb061fafac5976252d16ff800f
DevSoopark <40910757+DevSoopark@users.noreply.github.com>
2019-01-01 20:34:02 +0900

    Upload weather_app

---
+++ App.js
@@ -0,0 +1,80 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
+import Weather from "./Weather";
+
+const API_KEY = "0b89c5be7cf7f9c55626932c08b41437";
+
+export default class App extends Component {
+  state = {
+    isLoaded: false,
+    error: null,
+    temperature: null,
+    name: null
+  };
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this._getWeather(position.coords.latitude, position.coords.longitude);
+      },
+      error => {
+        this.setState({
+          error: error
+        });
+      }
+    );
+  }
+  _getWeather = (lat, long) => {
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&APPID=${API_KEY}`
+    )
+      .then(response => response.json())
+      .then(json => {
+        this.setState({
+          temperature: json.main.temp,
+          name: json.weather[0].main,
+          isLoaded: true
+        });
+      });
+  };
+  render() {
+    const { isLoaded, error, temperature, name } = this.state;
+    return (
+      <View style={styles.container}>
+        <StatusBar hidden={true} />
+        {isLoaded ? (
+          <Weather
+            weatherName={name}
+            temp={Math.ceil(temperature - 273.15)}
+          />
+        ) : (
+          <View style={styles.loading}>
+            <Text style={styles.loadingText}>Getting the fucking weather</Text>
+            {error ? <Text style={styles.errorText}>{error}</Text> : null}
+          </View>
+        )}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff"
+  },
+  errorText: {
+    color: "red",
+    backgroundColor: "transparent",
+    marginBottom: 40
+  },
+  loading: {
+    flex: 1,
+    backgroundColor: "#FDF6AA",
+    justifyContent: "flex-end",
+    paddingLeft: 25
+  },
+  loadingText: {
+    fontSize: 38,
+    marginBottom: 24
+  }
+});
